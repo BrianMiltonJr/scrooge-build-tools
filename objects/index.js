1ffd63cb27d021b03a77f474dbbc4ae70a314540
Brian Milton <harlanbmiltonjr@gmail.com>
2020-07-17 11:31:08 +0000

    Included Helper and Process Stream classes

--- objects/index.js
+++ objects/index.js
@@ -1,3 +1,5 @@
 exports.Addons = require('./addons').addons;
 exports.ConfigControl = require('./config_control').ConfigControl;
 exports.Server = require('./server').Server;
+exports.ProcessStream = require('./process_stream').ProcessStream;
+exports.Helper = require('./helper').Helper;
