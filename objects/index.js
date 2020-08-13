2c5c3b671b2c4246ab5a3f77516f7c5ccd566bd7
Brian Milton <harlanbmiltonjr@gmail.com>
2020-08-13 10:43:30 +0000

    Added new stuff
    includes an attempt at loading object files procedurally.
    
    As well as add our new ChatPacker Class

--- objects/index.js
+++ objects/index.js
@@ -1,5 +1,16 @@
+//An attempt at procedurally loading all these objects into the exports, will work on it later.
+// const fs = require('fs');
+
+// let files = fs.readdirSync(__dirname, {withFileTypes: false});
+// console.log(files);
+
+// files.map((file, index) => {
+//     let name = file.substr(0, file.lastIndexOf-3);
+//     exports[name] = require(`./${name}`);
+// });
 exports.Addons = require('./addons').addons;
 exports.ConfigControl = require('./config_control').ConfigControl;
 exports.Server = require('./server').Server;
 exports.ProcessStream = require('./process_stream').ProcessStream;
 exports.Helper = require('./helper').Helper;
+exports.ChatPacker = require('./chat_packer').ChatPacker;
