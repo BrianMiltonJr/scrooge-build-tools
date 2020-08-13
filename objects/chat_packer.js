c1082f0eef053c2d1b3acb4b680c377c430158fb
Brian Milton <harlanbmiltonjr@gmail.com>
2020-08-13 14:01:27 +0000

    Sped up Transform by streamlining iteration
    We used to iterate through the whole buffer array.
    
    find all escape sequence pos and human readable format codes.
    
    Then we iterated again to replace them then combine.
    
    We now walk through the buffer once and capturing everything
    as we go and concat all the new buffers at the end.
    
    Also commented for easy readability.

--- objects/chat_packer.js
+++ objects/chat_packer.js
@@ -1,65 +1,76 @@
 const Transform = require('stream').Transform;
 const sequences = require('./ansi_sequences.json');
 
+/*
+    A nifty implementation of a Transform Stream. When we pipe output into this stream we loop through
+    it to find escape sequences and replace them with their formatting counterpart. This is to make
+    parsing for react much easier. Since server programs tend to use ASCII escape sequences instead
+    of CSS color properties.
+
+    Can be sped up by maybe preconditioning the buffer to not process unless we can confirm escape
+    sequences so we aren't iterating through each byte in the streams
+*/
+
 class ChatPacker extends Transform {
 
     constructor() {
         super();
     }
 
+    //Transform our incoming stream and pushed it onwards
     _transform(chunk, enc, next) {
-        const ansiCodes = this.getAnsiCodes(chunk);
-        this.push(ansiCodes);
+        this.push(this.processChunk(chunk))
         next();
     }
 
-    getAnsiCodes(chunk) {
-        let sequencePass = false;
-        let currentCode = 0;
-        let codes = [];
+    processChunk(chunk) {
+        //What sequence are we currently parsing
+        let tempSequence = "";
+        //Our chunk gets split into smaller buffers with their Ansi Code replace with an easy 2 parse %colorFormat%
+        let formattedChunks = [];
 
+        //Start at the beginning of the buffer and work our way through it.
         for(let i = 0; i < chunk.length; i++) {
-            const byte = chunk[i];
+            //The current byte
+            let byte = chunk[i];
+            //Where our 2nd nested for loop will pick up to find the of this format codes reach
+            let pickup = null;
 
+            //Does this character start the escape sequence
             if(byte === 27) {
-                sequencePass = true;
-                codes[currentCode] = {start: i, sequence: "", end: null};
-                continue;
-            }
 
-            if(sequencePass) {
-                if(byte === 109) {
-                    sequencePass = false;
-                    codes[currentCode].end = i+1;
-                    currentCode += 1;
-                    continue;
+                //Work our way onwards adding every character to our tempSequence untill we reach m, once we do we set out pick up to the next byte
+                for(let j = i+1; j < chunk.length; j++) {
+                    if(chunk[j] !== 109) {
+                        tempSequence += chunk.toString("utf-8", j, j+1)
+                    } else {
+                        pickup = j+1
+                        break;
+                    }
                 }
-                codes[currentCode].sequence += chunk.toString("utf-8", i, i+1);
+
+                // We continue from our pickup then look for the next escape sequence. Once we find it we concat
+                // two buffers which consist of our formatting code followed by the slice of the buffer that follows
+                // onto the next escape sequence. We then set I to pick up where we left of on our primary loop
+                // We also reset tempSequence
+                for(let k = pickup; k < chunk.length; k++) {
+                    if(chunk[k] === 27) {
+                        console.log(tempSequence);
+                        formattedChunks.push(Buffer.concat([Buffer.from(`%${sequences[tempSequence]}%`), chunk.slice(pickup, k)]));
+                        i = k;
+                        tempSequence = "";
+                        break;
+                    }
+                }
             }
         }
 
-        if(codes.length > 0)
-            return this.replaceCodeInBuffer(codes, chunk);
+        // If we actually parsed escape sequences we concat all the sub Buffers into one buffer then return it
+        // else we return the original chunk unmodified
+        if(formattedChunks.length > 0)
+            return Buffer.concat(formattedChunks);
         else
-            return chunk;
-    }
-
-    replaceCodeInBuffer(codes, chunk) {
-        let tempParts = [];
-        for(let i = 0; i < codes.length; i++) {
-            let current = codes[i];
-            let ansiCode = sequences[current.sequence];
-            if(ansiCode === "reset")
-                continue;
-            
-            let next = codes[i+1];
-            if(next !== undefined)
-                tempParts.push(Buffer.concat([Buffer.from(`%${ansiCode}%`), chunk.slice(current.end, next.start)]));
-            else
-                tempParts.push(chunk.slice(current.end));
-        }
-
-        return Buffer.concat(tempParts);
+            return chunk
     }
  }
 
